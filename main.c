b589b181ae93d693a3b41a70d4b8e67c74243ada
Eli Brooks <elibrks@gmail.com>
2018-07-07 14:25:16 -0700

    added frequency check for input

--- main.c
+++ main.c
@@ -4,5 +4,22 @@
 
 int main(int argc, char *argv[]){
 
+  char str[20], ch;
+  printf("Enter the text to encode:");
+  gets(str);
+  printf("You want to encode the string '", c, "'\n");
+  scanf("%c",&ch);
+
+  for(i = 0; str[i] != '\0'; ++i){
+    if(ch == str[i]){
+        ++frequency;
+    }
+  }
+  printf("Frequency of %c = %d", ch, frequency);
+  int i, frequency = 0;
+
+
+
+
   return 0;
 }
