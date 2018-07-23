4a9ca00d253acd760d3f96a1deefa523393b6414
Eli Brooks <elibrks@gmail.com>
2018-07-23 11:55:50 -0700

    fixed some errors found with -Wall

--- demo.c
+++ demo.c
@@ -114,8 +114,8 @@ void optimal_encoding(int codes[26], int number_of_letters){
 
 
 //}
-bool safe_add(unsigned long long compressed_data, int b, unsigned long long compressed_data2) {
-  printf("compressed_data = %d\n", compressed_data);
+bool safe_add(unsigned long compressed_data, int b) {
+  printf("compressed_data = %lu\n", compressed_data);
   //printf("%d: b\n", b);
   //printf("half_int_max - a = %d\n", half_int_max-a);
   int half_int_max = INT_MAX/2;
@@ -129,7 +129,7 @@ bool safe_add(unsigned long long compressed_data, int b, unsigned long long comp
   return true;
 }
 
-void decompress2(int str_length, unsigned char decompressed_str[200], unsigned long long compressed_data, unsigned long long compressed_data2, char alpha[26]){
+void decompress2(int str_length, unsigned char decompressed_str[200], unsigned long compressed_data, unsigned long compressed_data2, char alpha[26]){
   int i = 0;
   int pos = str_length-1;
 
@@ -153,13 +153,13 @@ void decompress2(int str_length, unsigned char decompressed_str[200], unsigned l
   }
 }
 
-void add0bit(unsigned long long *compressed_data, int data_stream[100], int a){
+void add0bit(unsigned long *compressed_data, int data_stream[100], int a){
   *compressed_data <<= 1;
   data_stream[a + 1] = 0;
   printf("Added Bit 0 to data stream!!\n");
 }
 
-void add1bit(unsigned long long *compressed_data, int data_stream[100], int a){
+void add1bit(unsigned long *compressed_data, int data_stream[100], int a){
   *compressed_data <<= 1;
   *compressed_data += 1;
   data_stream[a + 1] = 1;
@@ -169,9 +169,9 @@ void add1bit(unsigned long long *compressed_data, int data_stream[100], int a){
 int main(int argc, char *argv[]) {
   //printf("%llu", ULONG_LONG_MAX);
 	int sorted_freq[26] = { 0 };
-	char str[200], ch;
+	char str[200];
 	int i, j, k = 0;
-	unsigned long long compressed_data = 0;
+	unsigned long compressed_data = 0;
 
 	printf("Enter the alphabet letters to encode: ");
 	gets(str);
@@ -224,12 +224,12 @@ int main(int argc, char *argv[]) {
   int codes[26] = { 0 };
   optimal_encoding(codes, number_of_letters);
 
-	unsigned long long bit_to_add = 0;
+	int bit_to_add = 0;
 	int index = 0;
 	int data_stream[100] = { 0 };
 	index++;
 	compressed_data = 1;
-  unsigned long long compressed_data2 = 0;
+  unsigned long compressed_data2 = 0;
 	printf("\nStart Bit '1' is added to the data stream\n");
 	data_stream[0] = compressed_data;
 	for (i = 0; i < str_length; i++) {
@@ -242,7 +242,7 @@ int main(int argc, char *argv[]) {
 					bit_to_add = codes[j];
 					compressed_data <<= 1;
           //***figure out how to handle overflow***
-          if(!safe_add(compressed_data, bit_to_add, compressed_data2)){
+          if(!safe_add(compressed_data, bit_to_add)){
             compressed_data2 = compressed_data;
             compressed_data = 1;
           }
@@ -258,7 +258,7 @@ int main(int argc, char *argv[]) {
 					bit_to_add = codes[j];
 					if (bit_to_add == 0b0) {
 						compressed_data += bit_to_add;
-            if(!safe_add(compressed_data, bit_to_add, compressed_data2)){
+            if(!safe_add(compressed_data, bit_to_add)){
               compressed_data2 = compressed_data;
               compressed_data = 1;
             }
@@ -267,13 +267,13 @@ int main(int argc, char *argv[]) {
 						bit_to_add >>= 1;
 					}
 					else if (bit_to_add == 0b01) {
-            if(!safe_add(compressed_data, bit_to_add, compressed_data2)){
+            if(!safe_add(compressed_data, bit_to_add)){
               compressed_data2 = compressed_data;
               compressed_data = 1;
             }
             add0bit(&compressed_data, data_stream, index);
             index++;
-            if(!safe_add(compressed_data, bit_to_add, compressed_data2)){
+            if(!safe_add(compressed_data, bit_to_add)){
               compressed_data2 = compressed_data;
               compressed_data = 1;
             }
@@ -282,13 +282,13 @@ int main(int argc, char *argv[]) {
 						bit_to_add >>= 1;
 					}
 					else if (bit_to_add == 0b11) {
-            if(!safe_add(compressed_data, bit_to_add, compressed_data2)){
+            if(!safe_add(compressed_data, bit_to_add)){
               compressed_data2 = compressed_data;
               compressed_data = 1;
             }
             add1bit(&compressed_data, data_stream, index);
             index++;
-            if(!safe_add(compressed_data, bit_to_add, compressed_data2)){
+            if(!safe_add(compressed_data, bit_to_add)){
               compressed_data2 = compressed_data;
               compressed_data = 1;
             }
@@ -407,7 +407,7 @@ int main(int argc, char *argv[]) {
 			}
 		}
 	}
-  printf("\nCompressed Data: %d" , compressed_data + compressed_data2);
+  printf("\nCompressed Data: %lu" , compressed_data + compressed_data2);
   printf("\nCompressed Data Binary: ");
   for (i = 0; i < index + 1; i++) {
 	  printf("%d", data_stream[i]);
@@ -428,7 +428,6 @@ int main(int argc, char *argv[]) {
 
   unsigned char decompressed_str[200] = { };
   int pos = str_length - 1;
-  int init_compressed_data = compressed_data;
   if(number_of_letters == 2){
     decompress2(str_length, decompressed_str, compressed_data, compressed_data2, alpha);
   }
@@ -561,7 +560,7 @@ int main(int argc, char *argv[]) {
 	  }
   }
   printf("\nOriginal String:	%s	Length: %d	Original Size: %d bits		Compressed Size: %d bits\n", str, str_length, str_length*8, index);
-  printf("Decompressed str:	%s\n", decompressed_str);
+  printf("Decompressed String:	%s\n", decompressed_str);
 
 
 
